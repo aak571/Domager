c12eb6aed6bb03e7654c5169da9474031bca042e
Aakash <aakash.kavi10@gmail.com>
2024-03-24 22:45:16 +0530

    React JS Components

---
+++ title.jsx
@@ -0,0 +1,9 @@
+const Title = () => {
+    return (
+        <div className="">
+            <h1 style={{ background: 'purple', color: 'white' }} className="text-center p-1">Domager</h1>
+        </div>
+    )
+}
+
+export default Title
